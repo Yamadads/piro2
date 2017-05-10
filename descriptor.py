5cc0b633e69c23b5f8836a55881dfeacfad1bb80
Yamadads <spawlak@yahoo.com>
2017-05-11 00:21:41 +0200

    distance function, hamming distance

--- descriptor.py
+++ descriptor.py
@@ -1,4 +1,5 @@
 from brisk import describe_point as brisk_describe
+import numpy as np
 
 
 def extract(image, keypoints):
@@ -9,4 +10,21 @@ def extract(image, keypoints):
 
 
 def distance(descriptor1, descriptor2):
-    return 0
+    int_d1 = int(descriptor1, 2)
+    int_d2 = int(descriptor2, 2)
+    min_distance = len(descriptor1)
+    for i in range(min_distance):
+        specific_distance = bin(int_d1 ^ int_d2).count('1')
+        if specific_distance <= min_distance:
+            min_distance = specific_distance
+        int_d2 = int_d2 >> 1 if int_d2 >= 0 else (int_d2 + 0x100000000) >> 1
+    return min_distance
+
+
+    # min_distance = len(descriptor2)
+    # for i in range(min_distance):
+    #     descriptor2 = descriptor2[-1] + descriptor2[:-1]
+    #     specific_distance = sum(d1 != d2 for d1, d2 in zip(descriptor1, descriptor2))
+    #     if specific_distance <= min_distance:
+    #         min_distance = specific_distance
+    # return min_distance
--- tester.py
+++ tester.py
@@ -1,1 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import descriptor
 
+
+def test_distance_method():
+    descriptor1 = "01110011000000000000000000000000000000000000000000000000000000000000000000000"
+    descriptor2 = "01110011000000000000000000000000000000000000000000000000000000000000000000000"
+    descriptor3 = "01000011000000000000000000000000000000000000000000000000000000000000000000000"
+    descriptor4 = "11100110000000000000000000000000000000000000000000000000000000000000000000000"
+    descriptor5 = "11000110000000000000000000000000000000000000000000000000000000000000000000000"
+    descriptor6 = "10100110000000000000000010000000000000000000000000000000000000000000000000000"
+
+    check(1, descriptor1, descriptor2, 0)
+    check(2, descriptor1, descriptor3, 2)
+    check(3, descriptor1, descriptor4, 0)
+    check(4, descriptor1, descriptor5, 1)
+    check(5, descriptor1, descriptor6, 2)
+
+
+def check(id, desc1, desc2, correct_value):
+    if descriptor.distance(desc1, desc2) != correct_value:
+        print("false" + id.__str__())
+
+
+def main():
+    for i in range(100000): #100000
+        test_distance_method()
+
+
+if __name__ == '__main__':
+    main()
